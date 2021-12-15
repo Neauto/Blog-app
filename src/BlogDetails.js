5cecfcb83d6ba37d5e6492bff914dbf34bb46194
Neauto <79352851+Neauto@users.noreply.github.com>
2021-12-15 19:11:33 +0545

    Update BlogDetails.js

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -3,11 +3,11 @@ import useFetch from './useFetch';
 
 const BlogDetails = () => {
 const { id } = useParams();
-const { data: blog, isPending, error } = useFetch('http://localhost:3000/blogs/' + id);
+const { data: blog, isPending, error } = useFetch('http://localhost:8080/blogs/' + id);
 const history = useHistory();
 
 const handleClick = () => {
-    fetch('http://localhost:3000/blogs/' + blog.id, {
+    fetch('http://localhost:8080/blogs/' + blog.id, {
         method: 'DELETE'
     }).then(() => {
         history.push('/');
